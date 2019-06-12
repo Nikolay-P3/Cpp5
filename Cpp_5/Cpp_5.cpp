aa4f3b578ae173d9fa973a8d09e4ba3691452a90
Nicholas Pankratov <48381479+pankratov35@users.noreply.github.com>
2019-06-12 10:44:02 +0300

    Add files via upload

---
+++ Cpp_5/Cpp_5.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+void main()
+{
+	char a[1000], chet[1000];
+	fstream kote("1.txt", ios::in);
+	if (!kote)
+	{
+		cout << "File not open.";
+		exit(1);
+	}
+	for (int i = 0;; i++)
+	{
+		kote >> a[i] >> chet[i];
+		cout << a[i] << ' ' << chet[i] << '\n';
+	}
+	cout << chet[2];
+	system("pause");
+}
